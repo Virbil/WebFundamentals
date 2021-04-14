ac9bbabebfb59933210d2b26d332e7af3a177812
Bryce Eaton <bryce.eaton86@gmail.com>
2021-04-13 20:42:59 -0500

    Adding Playing with Objects

---
+++ WebFundamentals/JavaScript/playingWithObjects/playing-with-objects.js
@@ -0,0 +1,15 @@
+var users = [{name: "Michael", age:37}, {name: "John", age:30}, {name: "David", age:27}];
+
+console.log(users[1].age);
+
+// for (var user in users) {
+//   if (users[user].name == "John") {
+//       console.log(users[user].age);
+//   }    
+// }
+
+console.log(users[0].name);
+
+for (var user in users) {
+	console.log(users[user].name + ' - ' + users[user].age);   
+}
